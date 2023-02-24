5cb47f74d5f115c5bdada948836645e05acca7c9
zishanHow <111298322+zishanHow@users.noreply.github.com>
2023-02-25 02:07:53 +0600

    getting color scheme value.

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-console.log("Hello World")
 
-
+const colorValue = document.getElementById("color-input")
+const chooseMode = document.getElementById("choose-mode")
 
 const btn = document.getElementById("btn")
+const getRenderColor = document.getElementById("render-color-section")
 
 
 btn.addEventListener("click", () => {
-    const colorValue = document.getElementById("color-input").value
-    console.log(colorValue)
+    fetch(`https://www.thecolorapi.com/scheme?hex=${colorValue.value.slice(1)}&mode=${chooseMode.value}&count=5`)
+    .then((response) => response.json())
+    .then((data) => {
+        //console.log(data)
+        let colorArray = []
+        data.colors.forEach((item) => {
+           colorArray.push(item.hex.value)
+        })
+        console.log(colorArray)
+    })
 })
