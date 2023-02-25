29542b1298ed654ba5af124c9a03a1417c9fa078
zishanHow <111298322+zishanHow@users.noreply.github.com>
2023-02-26 01:04:51 +0600

    final design

--- index.js
+++ index.js
@@ -1,54 +1,46 @@
 
 const colorValue = document.getElementById("color-input")
 const chooseMode = document.getElementById("choose-mode")
-
 const btn = document.getElementById("btn")
 const renderColorSection = document.getElementById("render-color-section")
 const footerEl = document.getElementById("footer")
 
-
-function getScheme(){
+function getScheme() {
     fetch(`https://www.thecolorapi.com/scheme?hex=${colorValue.value.slice(1)}&mode=${chooseMode.value}&count=5`)
-    .then((response) => response.json())
-    .then((data) => {
-        //console.log(data)
-        let colorArray = []
-        data.colors.forEach((item) => {
-           colorArray.push(item.hex.value)
+        .then((response) => response.json())
+        .then((data) => {
+            //console.log(data)
+            let colorArray = []
+            data.colors.forEach((item) => {
+                colorArray.push(item.hex.value)
+            })
+            //console.log(colorArray)
+            renderColorDiv(colorArray)
+            renderFooter(colorArray)
         })
-        //console.log(colorArray)
-        renderColorDiv(colorArray)
-        renderFooter(colorArray)
-    })
 }
 
-// btn.addEventListener("click", () => {
-//     getScheme()
-// })
-
-
-
 document.addEventListener("click", (e) => {
-    if(e.target.id === "btn"){
+    if (e.target.id === "btn") {
         //console.log(e.target.id)
         getScheme()
     }
-    else if(e.target.dataset.hex || e.target.id === "color-name"){
-        console.log(e.target.dataset.hex)
+    else if (e.target.dataset.hex || e.target.id === "color-name") {
+        //console.log(e.target.dataset.hex)
         navigator.clipboard.writeText(e.target.dataset.hex)
             .then(() => {
-                console.log("Text Copid to clipboard")
+                //console.log("Text Copid to clipboard")
                 alert("Text Copid to clipboard")
             })
             .catch((error) => {
                 console.error("Failed to copy text: ", error)
             })
     }
-    else if(e.target.dataset.fhex){
-        console.log(e.target.dataset.fhex)
+    else if (e.target.dataset.fhex) {
+        //console.log(e.target.dataset.fhex)
         navigator.clipboard.writeText(e.target.dataset.fhex)
             .then(() => {
-                console.log("Text Copid to clipboard")
+                //console.log("Text Copid to clipboard")
                 alert("Text Copid to clipboard")
             })
             .catch((error) => {
@@ -57,8 +49,7 @@ document.addEventListener("click", (e) => {
     }
 })
 
-
-function renderColorDiv(color){
+function renderColorDiv(color) {
     //console.log(color)
     let colorHtml = ``
 
@@ -72,8 +63,6 @@ function renderColorDiv(color){
     })
     renderColorSection.innerHTML = colorHtml
 }
-
-
 
 function renderFooter(color) {
     let footerHtml = ``
