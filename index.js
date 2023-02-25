6c6f08dd6c24ac896a2ebe600b28dc88747c716d
zishanHow <111298322+zishanHow@users.noreply.github.com>
2023-02-26 01:15:50 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,31 @@
-
 const colorValue = document.getElementById("color-input")
 const chooseMode = document.getElementById("choose-mode")
 const btn = document.getElementById("btn")
 const renderColorSection = document.getElementById("render-color-section")
 const footerEl = document.getElementById("footer")
 
+// getting the color scheme from API
 function getScheme() {
     fetch(`https://www.thecolorapi.com/scheme?hex=${colorValue.value.slice(1)}&mode=${chooseMode.value}&count=5`)
         .then((response) => response.json())
         .then((data) => {
-            //console.log(data)
             let colorArray = []
             data.colors.forEach((item) => {
                 colorArray.push(item.hex.value)
             })
-            //console.log(colorArray)
             renderColorDiv(colorArray)
             renderFooter(colorArray)
         })
 }
 
+// handeling the click event 
 document.addEventListener("click", (e) => {
     if (e.target.id === "btn") {
-        //console.log(e.target.id)
         getScheme()
     }
     else if (e.target.dataset.hex || e.target.id === "color-name") {
-        //console.log(e.target.dataset.hex)
         navigator.clipboard.writeText(e.target.dataset.hex)
             .then(() => {
-                //console.log("Text Copid to clipboard")
                 alert("Text Copid to clipboard")
             })
             .catch((error) => {
@@ -37,10 +33,8 @@ document.addEventListener("click", (e) => {
             })
     }
     else if (e.target.dataset.fhex) {
-        //console.log(e.target.dataset.fhex)
         navigator.clipboard.writeText(e.target.dataset.fhex)
             .then(() => {
-                //console.log("Text Copid to clipboard")
                 alert("Text Copid to clipboard")
             })
             .catch((error) => {
@@ -49,26 +43,22 @@ document.addEventListener("click", (e) => {
     }
 })
 
+// rendering the color to the DOM.
 function renderColorDiv(color) {
-    //console.log(color)
     let colorHtml = ``
-
     color.forEach((unit) => {
-        //console.log(unit)
         colorHtml += ` 
-                <div data-hex='${unit}' class="rendering-color" id="rendering-color" style="background-color:${unit};">
-                    
-                </div>
+            <div data-hex='${unit}' class="rendering-color" id="rendering-color" style="background-color:${unit};">
+            </div>
         `
     })
     renderColorSection.innerHTML = colorHtml
 }
 
+// color hex number at bottom.
 function renderFooter(color) {
     let footerHtml = ``
-
     color.forEach((unit) => {
-        //console.log(unit)
         footerHtml += `
             <p data-fhex='${unit}' class="ftr-p" id="ftr-p">${unit}</p>
         `
